72d9b29701093d410f7ed94ce8860d4897223b0d
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-10-26 01:16:53 -0500

    added a controller for dealing with messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ChiText::Application.routes.draw do
+  get "messages/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
