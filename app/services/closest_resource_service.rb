e1bbecde135a1aa7d42123b60cb25aa9d41a43aa
bosgood <bosgood@gmail.com>
2013-10-26 03:47:27 -0500

    Service for querying closest resource of a given type based on latitude/longitude

---
+++ app/services/closest_resource_service.rb
@@ -0,0 +1,28 @@
+class ClosestResourceService
+  class << self
+    def closest_of_type(location_type, latitude, longitude)
+      locations = Location.where(location_type: location_type)
+      if locations.length == 0
+        return nil
+      end
+
+      nearest = nil
+      nearest_distance = -1
+      
+      # Brute force search for the closest location (!)
+      locations.each do |loc|
+        distance = LocationHelper.distance(
+          latitude.to_f, longitude.to_f, loc.latitude.to_f, loc.longitude.to_f
+        )
+        
+        # Is first location or found closer location
+        if nearest.nil? or nearest_distance > distance
+          nearest = loc
+          nearest_distance = distance
+        end
+      end
+      
+      nearest
+    end
+  end
+end
