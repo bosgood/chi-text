5b27522850b31c377dbe222c7fec933fd92be282
bosgood <bosgood@gmail.com>
2013-10-26 11:02:30 -0500

    Centralizing some message sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,14 @@
 class MessagesController < ApplicationController
   include MessagesHelper
-  
+
   def index
-    binding.pry
-    return nil if params[:Body].nil? or params[:From].nil?
-    keyword = params[:Body].split(' ').first
-    if keyword == 'directions'
-      ds = DirectionsService.new
-      match = params[:Body].match(/.*start\:(?<start>.+)end\:(?<end>.+)/)
-      send_message ds.get_step_by_step_directions(match[:start], match[:end]).join('*')
-    end
+    body = params[:Body]
+    return nil if body.nil? or params[:From].nil?
+    replier = get_reply_handler(get_message_keyword(body))
+    message = replier.call(body)
+    send_message params[:From], message
   end
-  
-  def send_message(body)
-    MessageService.send_message(params[:From], body)
-  end
-
 end
-
-
 
 =begin
 Parameters: {"AccountSid"=>"AC11e679873f0a78342c14cba2d6250e70", "MessageSid"=>"SM8bc912b816bc6d25ae22983ad09383ee", "Body"=>"hey", "ToZip"=>"60603", "ToCity"=>"CHICAGO", "FromState"=>"IL", "ToState"=>"IL", "SmsSid"=>"SM8bc912b816bc6d25ae22983ad09383ee", "To"=>"+13126983244", "ToCountry"=>"US", "FromCountry"=>"US", "SmsMessageSid"=>"SM8bc912b816bc6d25ae22983ad09383ee", "ApiVersion"=>"2010-04-01", "FromCity"=>"TINLEY PARK", "SmsStatus"=>"received", "NumMedia"=>"0", "From"=>"+17086207839", "FromZip"=>"60428"}
