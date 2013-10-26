0afefc0f09ada1434e64a0e2b8cd617fdf1165e8
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-10-25 20:44:27 -0500

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChiText::Application.config.secret_key_base = 'e49b7d1281e36ba42b4928148e8e78a91780370d7715312cf265c87375548c46e8e092faba1fd117eabb2fade942912dc97e26ea680a07f8b093437ddad9fd0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChiText::Application.config.session_store :cookie_store, key: '_chi-text_session'
