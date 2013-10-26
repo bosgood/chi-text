1c97daaf09f3d6f6af66c55685d4b56c46aa924f
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-10-26 09:59:48 -0500

    added a directions message reciever

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,21 @@
 class MessagesController < ApplicationController
+  
   def index
-    puts TW.inspect
-    puts params
+    return nil if params[:Body].nil? or params[:From].nil?
+    keyword = params[:Body].split(' ').first
+    if keyword == 'directions'
+      ds = DirectionsService.new
+      match = params[:Body].match(/.*start\:(?<start>.+)end\:(?<end>.+)/)
+      send ds.get_step_by_step_directions(match[:start], match[:end])
+    end
+  end
+  def send body
+    TW.account.message.create({from: '+13126983244', to: params[:From], body: body})
   end
 end
+
+
+
+=begin
+Parameters: {"AccountSid"=>"AC11e679873f0a78342c14cba2d6250e70", "MessageSid"=>"SM8bc912b816bc6d25ae22983ad09383ee", "Body"=>"hey", "ToZip"=>"60603", "ToCity"=>"CHICAGO", "FromState"=>"IL", "ToState"=>"IL", "SmsSid"=>"SM8bc912b816bc6d25ae22983ad09383ee", "To"=>"+13126983244", "ToCountry"=>"US", "FromCountry"=>"US", "SmsMessageSid"=>"SM8bc912b816bc6d25ae22983ad09383ee", "ApiVersion"=>"2010-04-01", "FromCity"=>"TINLEY PARK", "SmsStatus"=>"received", "NumMedia"=>"0", "From"=>"+17086207839", "FromZip"=>"60428"}
+=end
