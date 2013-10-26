ac41ccd04d5d4c602026d1356dabde80c988e884
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-10-26 13:12:46 -0500

    hopfully fixed a 500 error

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class MessagesController < ApplicationController
+      skip_before_filter  :verify_authenticity_token
       protect_from_forgery with: :null_session
       respond_to :json
       include MessagesHelper
