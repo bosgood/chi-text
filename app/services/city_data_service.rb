7cb17d1b84d93bc07ae1ba5475fbfc0184594aac
bosgood <bosgood@gmail.com>
2013-11-19 20:33:30 -0600

    Implementing basic version of cityofchicago.org service

--- app/services/city_data_service.rb
+++ app/services/city_data_service.rb
@@ -1,23 +1,24 @@
 require 'net/https'
 
 class CityDataService
-  CHICAGO_API_URL = 'https://data.cityofchicago.org/resource/'
+  CHICAGO_API_URL = 'https://data.cityofchicago.org/resource'
+  class << self
+    def make_data_request(resource)
+      uri = URI("#{CHICAGO_API_URL}/#{resource}")
 
-  def make_data_request(resource, params)
-    uri = URI("#{CHICAGO_API_URL}/#{resource}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request.add_field('X-App-Token', ENV['CITY_OF_CHICAGO_APP_TOKEN'])
 
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request.add_field('X-App-Token', ARGV[0])
+      http.request(request)
+    end
 
-    http.request(request)
-  end
-
-  def get_flu_shot_locations
-    url = 'flu-shot-clinic-locations-2013.json?recall_id=94'
-    make_data_request(url)
+    def get_flu_shot_locations
+      url = 'flu-shot-clinic-locations-2013.json'
+      make_data_request(url)
+    end
   end
 end
